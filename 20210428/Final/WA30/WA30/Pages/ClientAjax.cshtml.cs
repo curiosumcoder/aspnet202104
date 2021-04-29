9bf837fac67dcf04bcb3b7045cf2d6805815670d
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-04-28 18:00:31 -0600

    Actualización

---
+++ 20210428/Final/WA30/WA30/Pages/ClientAjax.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WA30.Pages
+{
+    public class ClientAjaxModel : PageModel
+    {
+
+        public void OnGet() 
+        { 
+        }
+    }
+}
