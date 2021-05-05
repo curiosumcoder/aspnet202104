b4afb985ad59b07ee3ad5a5022e94528cf282ca7
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-05-05 14:29:49 -0600

    Actualización

---
+++ 20210505/Inicio/WA30/WA30/Pages/ClientVue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WA30.Pages
+{
+    public class ClientVueModel : PageModel
+    {
+
+        public void OnGet() 
+        { 
+        }
+    }
+}
