96dfb7902d4752e2c57269679cf596dc892b4665
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-05-05 14:49:01 -0600

    Actualización

--- 20210428/Final/WA30/WA30/wwwroot/js/product.js
+++ 20210428/Final/WA30/WA30/wwwroot/js/product.js
@@ -1,14 +1,44 @@
-﻿document.addEventListener('DOMContentLoaded', () => {
+﻿// Se requiere incluir en el documento la biblioteca underscore.js
+document.addEventListener('DOMContentLoaded', () => {
     console.log('Document ready!');
 
-    let ps = new ProductService();
+    const filter = document.getElementById('filter');
+    const bSearch = document.getElementById('bSearch');
+    bSearch.addEventListener('click', (event) => {
+        event.preventDefault();
 
-    ps.getProducts().then((data) => {
-        console.log(data);
-    }).catch((err) => {
-        console.log(`Error: ${err}`);
+        let ps = new ProductService();
+
+        ps.getProducts().then((data) => {
+            //console.log(data);
+
+            let filtered = _.filter(data, function (p) {
+                return p.productName.toLowerCase().
+                    indexOf(filter.value.toLowerCase()) !== -1;
+            });
+
+            //console.log(filtered);
+
+            var grouped = _.groupBy(filtered, "categoryID");
+            console.log(grouped);
+
+            for (var key in grouped) {
+                var items = grouped[key];
+
+                console.log(grouped[key][0].category.categoryName);
+                appendCategory(grouped[key][0].category.categoryName);
+
+                items.forEach(function (item, index, array) {
+                    console.log(item);
+
+                    appendProduct(item);
+                });
+            };
+
+        }).catch((err) => {
+            console.log(`Error: ${err}`);
+        });
     });
-
 });
 
 function appendProduct(item) {
--- 20210505/Final/WA30/WA30/wwwroot/js/app.js
+++ 20210505/Final/WA30/WA30/wwwroot/js/app.js
@@ -1,37 +1,66 @@
-﻿let app = new Vue({
-    el: '#app',
-    data: {
-        filter: '',
-        items: []
+﻿const app = Vue.createApp({
+    data() {
+        return {
+            filter: '',
+            items: []
+        }
     },
     methods: {
-        search: function () {
+        search: async function () {
             console.log(`Searching for ${this.filter}`);
-            let self = this;
-            fetch('js/index.json').then(
-                function (response) {
-                    if (response.status !== 200) {
-                        console.log('Looks like there was a problem. Status Code: ' +
-                            response.status);
-                        return;
-                    }
-                    response.json().then(function (data) {
-                        let filtered = data.filter(p => p.productName.toLowerCase().indexOf(
-                            self.filter.toLowerCase()) !== -1);
-                        let grouped = filtered.reduce((result, next) => {
-                            let key = next.category.categoryName;
-                            if (!result[key]) {
-                                result[key] = [];
-                            }
-                            result[key].push(next);
-                            return result;
-                        }, {});
-                        self.items = grouped;
-                    });
+
+            let response = await fetch('js/index.json');
+
+            if (response.status !== 200) {
+                console.error(`Looks like there was a problem. Status Code: ${response.status}`);
+                return;
+            }
+
+            let products = await response.json();
+
+            let filtered = products.filter(p => p.productName.toLowerCase().indexOf(
+                this.filter.toLowerCase()) !== -1);
+
+            let grouped = filtered.reduce((result, next) => {
+                let key = next.category.categoryName;
+                if (!result[key]) {
+                    result[key] = [];
                 }
-            ).catch(function (err) {
-                console.log('Fetch Error :-S', err);
-            });
+                result[key].push(next);
+                return result;
+            }, {});
+
+            this.items = grouped;
+            console.log(grouped);
         }
     }
 });
+
+const vm = app.mount('#app');
+
+function noAsync() {
+    fetch('js/index.json').then(
+        function (response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+            response.json().then(function (data) {
+                let filtered = data.filter(p => p.productName.toLowerCase().indexOf(
+                    self.filter.toLowerCase()) !== -1);
+                let grouped = filtered.reduce((result, next) => {
+                    let key = next.category.categoryName;
+                    if (!result[key]) {
+                        result[key] = [];
+                    }
+                    result[key].push(next);
+                    return result;
+                }, {});
+                self.items = grouped;
+            });
+        }
+    ).catch(function (err) {
+        console.log('Fetch Error :-S', err);
+    });
+}
