97def0d7eceefb49ea3e91f867699c0014dee0b7
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-05-10 20:50:02 -0600

    Introducción a Entity Framework Core

---
+++ 20210510/CA4/CA4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace CA4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NWContext())
+            {
+                var products = db.Products.ToList();
+
+                foreach (var p in products)
+                {
+                    Console.WriteLine($"{p.ProductId}, {p.ProductName}, {p.UnitPrice}");
+                }
+
+                // Change Tracking
+                var p1 = products.Single(p => p.ProductId == 1);
+
+                p1.ProductName += "M";
+
+                var newProduct = new Product() { ProductName = "Demostración" };
+
+                db.Products.Add(newProduct);
+
+                var deleteProduct = products.Single(p => p.ProductId == 78);
+                //db.Remove(deleteProduct);
+                db.Products.Remove(deleteProduct);
+
+                db.SaveChanges();
+            }
+
+            Console.WriteLine("READY!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 20210510/CA4/CA41/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CA41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthwindContext())
+            {
+                foreach (var p in db.Products)
+                {
+                    Console.WriteLine(p.ProductName);
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
