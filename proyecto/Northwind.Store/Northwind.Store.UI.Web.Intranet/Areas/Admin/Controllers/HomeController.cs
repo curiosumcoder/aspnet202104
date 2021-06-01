14b90cb44ca6765bd7f45c312fe6a00bae8c4b40
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-05-31 19:49:32 -0600

    Auth y Areas

---
+++ proyecto/Northwind.Store/Northwind.Store.UI.Web.Intranet/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Store.UI.Web.Intranet.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles ="Admin, Conta")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
