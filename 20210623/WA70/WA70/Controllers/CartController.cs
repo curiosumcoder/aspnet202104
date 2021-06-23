3ce6a5a35304dd307ac70f020d265df27c99c605
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-06-23 17:58:00 -0600

    Demostración

---
+++ 20210623/WA70/WA70/Controllers/CartController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Store.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA70.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly NWContext _db;
+        private readonly SessionSettings _ss;
+
+        public CartController(NWContext db, SessionSettings ss)
+        {
+            _db = db;
+            _ss = ss;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_ss.Cart);
+        }
+
+        public ActionResult Add(int? id)
+        {
+            if (id.HasValue)
+            {
+                #region Session
+                var p = _db.Products.Find(id);
+                var cart = _ss.Cart;
+
+                cart.Items.Add(p);
+
+                _ss.Cart = cart;
+                #endregion
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
