a9de601b05e436dacd4730af56486f9b4ced0942
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-06-23 18:43:42 -0600

    TempData

--- 20210623/WA70/WA70/Controllers/CartController.cs
+++ 20210623/WA70/WA70/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Northwind.Store.Data;
+using Northwind.Store.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +13,32 @@ namespace WA70.Controllers
     {
         private readonly NWContext _db;
         private readonly SessionSettings _ss;
+        private readonly RequestSettings _rs;
 
         public CartController(NWContext db, SessionSettings ss)
         {
             _db = db;
             _ss = ss;
+            _rs = new RequestSettings(this);
         }
 
         public IActionResult Index()
         {
+            var productId = TempData[nameof(Product.ProductId)];            
+            var productName = TempData[nameof(Product.ProductName)];
+            //TempData.Keep(nameof(Product.ProductName));
+            //var productName = TempData.Peek(nameof(Product.ProductName));
+            //var productAdded = _rs.ProductAdded;
+
+            ViewBag.productAdded = _rs.ProductAdded;
+            //TempData.Keep();
+
             return View(_ss.Cart);
         }
 
         public ActionResult Add(int? id)
         {
+
             if (id.HasValue)
             {
                 #region Session
@@ -36,9 +49,16 @@ namespace WA70.Controllers
 
                 _ss.Cart = cart;
                 #endregion
+
+                #region TempData
+                TempData[nameof(Product.ProductId)] = p.ProductId;
+                TempData[nameof(Product.ProductName)] = p.ProductName;
+
+                _rs.ProductAdded = p;
+                #endregion
             }
 
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", "CX");
         }
     }
 }
--- 20210623/WA70/WA70/Settings/RequestSettings.cs
+++ 20210623/WA70/WA70/Settings/RequestSettings.cs
@@ -1,26 +1,26 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Northwind.Store.Model;
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Store.Model;
 
-//namespace WA70
-//{
-//    public class RequestSettings
-//    {
-//        readonly Controller _c;
-//        public RequestSettings(Controller c)
-//        {
-//            _c = c;
-//        }
+namespace WA70
+{
+    public class RequestSettings
+    {
+        readonly Controller _c;
+        public RequestSettings(Controller c)
+        {
+            _c = c;
+        }
 
-//        public Product ProductAdded
-//        {
-//            get
-//            {
-//                return _c.TempData.GetFromJson<Product>(nameof(ProductAdded)); ;
-//            }
-//            set
-//            {
-//                _c.TempData.SetAsJson(nameof(ProductAdded), value);
-//            }
-//        }
-//    }
-//}
+        public Product ProductAdded
+        {
+            get
+            {
+                return _c.TempData.GetFromJson<Product>(nameof(ProductAdded)); ;
+            }
+            set
+            {
+                _c.TempData.SetAsJson(nameof(ProductAdded), value);
+            }
+        }
+    }
+}
